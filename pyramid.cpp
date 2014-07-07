3f8166ae356705841d8158d78912172899fce568
Michael Moore <vmpiro11@yahoo.com>
2014-07-06 21:03:22 -0400

    adding .cpp file

---
+++ pyramid.cpp
@@ -0,0 +1,60 @@
+/*******************************************************
+* FILENAME : pyramid.cpp							   *
+* PROGRAMMERS : Jeremy McClelland &Michael Moore	   *
+* PURPOSE : Creates a pyramid with astericks based 	   *
+* on the int provided by the user					   *
+*******************************************************/
+
+// includes
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+
+int main()
+{
+	//variables
+	int i, j, k, n;
+	string bigNumber = "Y";
+
+	//begin main
+	cout << " Thank you for trying the Pyramid program v1.1 " << endl << endl;
+	system("pause"); 
+	cout << endl << " Let's create an awesome pyramid! How many rows would you like to have? " << endl;
+	cin >>  n;
+	cout << endl;
+
+	if (n > 35) 
+	{
+		while (bigNumber == "Y")
+		{ //begin n if
+			cout << " Wow! That's a large number. This could end up being a crazy pyramid. " << endl;
+			cout << " Would you like to enter a lower number? Y/N" << endl;
+			cin >> bigNumber;
+			if (bigNumber == "Y")
+			{
+				cout << endl << " Ok. What number would you like to use instead then? " << endl;
+				cin >> n;
+				bigNumber = "N";
+				cout << endl << endl;
+			} //end if
+		} //end while
+	} //end n if
+
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 0; j < (n - i); j++)
+				cout << " ";
+			for (j = 1; j <= i; j++)
+				cout << "*";
+			for (k = 1; k < i; k++)
+				cout << "*";
+				cout << endl;
+		} // end for
+
+	cout << endl << endl << " Thank you for trying out pyramid v1.1! " << endl << endl;
+	system("pause");
+	return 0;
+}
